43920d82ddc6e06657c40076a3029f10eca1a2db
Robert Parrillo <robertparrillo@Roberts-MacBook-Pro.local>
2017-06-20 10:19:28 -0500

    installed postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170618145613) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "com_payments", force: :cascade do |t|
     t.string   "card_type"
     t.string   "number"
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 20170618145613) do
     t.string   "video_content_type"
     t.integer  "video_file_size"
     t.datetime "video_updated_at"
-    t.         "video_meta"
+    t.text     "video_meta"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,8 +57,8 @@ ActiveRecord::Schema.define(version: 20170618145613) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "votes", force: :cascade do |t|
